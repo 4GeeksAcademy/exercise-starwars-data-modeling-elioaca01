4551c3a433ce33a4e4078aaa9f9245690f229175
elioaca01 <eioaca.06@gmail.com>
2024-12-05 04:17:44 +0000

    data model starwars

--- src/models.py
+++ src/models.py
@@ -1,32 +1,54 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String,Boolean,Table
 from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+favorites=Table(
+    "favorites",
+    Base.metadata,
+    Column("user.id",ForeignKey("user.id")),
+    Column("planet.id",ForeignKey("planet.id")),
+    Column("character.id",ForeignKey("character.id"))
+)
+
+class User(Base):
+    __tablename__ = 'user'
+    id = Column(Integer, primary_key=True)
+    username = Column(String(40), nullable=False, unique=True)
+    password = Column(String(10), nullable=False)
+
+    planets = relationship('Planet',secondary=favorites, back_populates='user')
+    characters = relationship('Character',secondary=favorites, back_populates='user')
+
+
+class Planet(Base):
+    __tablename__ = 'planet'
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    description = Column(String(250),nullable=False)
+    climate = Column(String(50))
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+    user = relationship('User',secondary=favorites, back_populates='planet')
+    characters = relationship('Character',secondary=favorites, back_populates='planet')
+    
+
+
+class Character(Base):
+    __tablename__='character'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    name = Column(String(50),nullable=False)
+    alive = Column(Boolean,nullable=False)
 
-    def to_dict(self):
-        return {}
+    user = relationship('User',secondary=favorites, back_populates='character')
+    planets = relationship('Planet',secondary=favorites, back_populates='character')
+
+
+
+
 
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
